a7947110beb5d63e1d36c73d321210894190127f
sofochka-41 <ks_s2000@mail.ru>
2023-02-26 21:57:01 +0300

    Бесконечные числа Фибоначчи

---
+++ task_13.py
@@ -0,0 +1,28 @@
+import itertools 
+class Fib:    
+    """По объектам этого класса можно итерироваться и получать чисела Фибоначчи"""
+
+    class _Fib_iter:
+        """Внутренний класс — итератор"""
+        def __init__(self):
+            self.i = 0
+            self.j = 1
+        
+        def __next__(self):
+            if self.i == 4:
+                raise StopIteration()
+            else: 
+                self.j = self.i + self.j
+                self.i = self.j - self.i
+                return self.j 
+
+    def __iter__(self):
+        """Создать и вернуть итератор"""
+        return Fib._Fib_iter()
+
+fibb = Fib()
+for f in fibb:
+    print (f)
+    if f>600:
+        break
+    
